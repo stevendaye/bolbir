41627414ec4126da59f14e7c97bd844514eaad7b
Steven <dev.stevendaye@yahoo.com>
2019-01-16 20:30:14 +0100

    Modified search results landing URL

--- client/list.js
+++ client/list.js
@@ -2,7 +2,7 @@
 const list = [
   {
     title: "Where Are The Dead",
-    url: "https://www.jw.org/search=where+are+the+dead",
+    url: "https://www.jw.org/en/search/?q=where+are+the+dead",
     head: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...",
     author: "JW",
     num_comments: 245,
@@ -11,7 +11,7 @@ const list = [
   },
   {
     title: "Will Suffering Ever End",
-    url: "https://www.jw.org/search=will+suffering+end",
+    url: "https://www.jw.org/en/search/?q=will+suffering+end",
     head: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum...",
     author: "JW",
     num_comments: 945,
@@ -20,7 +20,7 @@ const list = [
   },
   {
     title: "Young People Ask",
-    url: "https://www.jw.org/search=young+people+ask",
+    url: "https://www.jw.org/en/search/?q=young+people+ask",
     head: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo...",
     author: "JW",
     num_comments: 405,
@@ -29,7 +29,7 @@ const list = [
   },
   {
     title: "What is God's Kingdom",
-    url: "https://www.jw.org/search=what+is+God+kingdom",
+    url: "https://www.jw.org/en/search/?q=what+is+God+kingdom",
     head: "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus...",
     author: "JW",
     num_comments: 876,
@@ -38,7 +38,7 @@ const list = [
   },
   {
     title: "What is God's Name",
-    url: "https://www.jw.org/search=what+is+God+name",
+    url: "https://www.jw.org/en/search/?q=what+is+God+name",
     head: "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem...",
     author: "JW",
     num_comments: 987,
@@ -47,7 +47,7 @@ const list = [
   },
   {
     title: "Why Do People Grow Old and Die",
-    url: "https://www.jw.org/search=why+people+die",
+    url: "https://www.jw.org/en/search/?q=why+people+die",
     head: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident...",
     author: "JW",
     num_comments: 745,
@@ -56,7 +56,7 @@ const list = [
   },
   {
     title: "How Do You View The Future",
-    url: "https://www.jw.org/search=how+do+you+view+the+future",
+    url: "https://www.jw.org/en/search/?q=how+do+you+view+the+future",
     head: "Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat...",
     author: "JW",
     num_comments: 197,
