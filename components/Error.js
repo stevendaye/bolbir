d8e54fbc9f38dde521c61396d3a3a864a2fa6cc2
Steven <dev.stevendaye@yahoo.com>
2019-01-20 00:40:47 +0100

    Added Error functionality

---
+++ components/Error.js
@@ -0,0 +1,10 @@
+/* Creating the Error component */
+import React from "react";
+
+const Err = () =>
+  <div className = "error">
+    Oups, Something went wrong!<br/>
+    Please, check your internet connection or update your date!
+  </div>
+
+export default Err;
