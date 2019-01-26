0b7229716549263f13de7dc9e0d395285d821bfe
Steven <dev.stevendaye@yahoo.com>
2019-01-26 04:10:02 +0100

    Added a ref attribute to access the DOM API

--- components/Search.js
+++ components/Search.js
@@ -3,25 +3,37 @@ import React from "react";
 import Button from "./Button";
 import PropTypes from "prop-types";
 
-const Search = ({ value, onChange, placeholder, type, onSubmit, onClick, children }) =>
-  <div className = "search-box-wrapper">
-    <form onSubmit = {onSubmit} >
-      <input
-        className = "search-box"
-        value= {value}
-        type = {type}
-        placeholder = {placeholder}
-        onChange = {onChange}
-      />
-      <Button
-        type = "submit"
-        className = "search-button"
-        onClick = {onClick}
-      >
-        {children}
-      </Button>
-    </form>
-  </div>
+class Search extends React.Component {
+  componentDidMount () {
+    this.searchInput && this.searchInput.focus();
+  }
+
+  render () {
+    const { value, onChange, placeholder, type, onSubmit, onClick, children } = this.props;
+
+    return (
+      <div className = "search-box-wrapper">
+        <form onSubmit = {onSubmit} >
+          <input
+            className = "search-box"
+            value= {value}
+            type = {type}
+            placeholder = {placeholder}
+            onChange = {onChange}
+            ref = {el => this.searchInput = el}
+          />
+          <Button
+            type = "submit"
+            className = "search-button"
+            onClick = {onClick}
+          >
+            {children}
+          </Button>
+        </form>
+      </div>
+    );
+  }
+}
 
 // Checking Search Props type;
 Search.propTypes = {
