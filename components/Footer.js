2abf1bb2d1f36ae3f05b6caa8ed1abd25c1512b6
Steven <dev.stevendaye@yahoo.com>
2019-01-26 04:06:26 +0100

    Added a Footer Component

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+/* Creating the Footer Component */
+import React from "react";
+
+const Footer = () =>
+  <footer>
+    <div>
+      <p>&copy; 2019 All Right Reserved</p>
+      <p><a href = "#">Rules and Regulations</a></p>
+      <p><a href = "#">Privacy</a></p>
+      <p>Made with &#10084; by Steven Daye</p>
+    </div>
+  </footer>
+
+export default Footer;
