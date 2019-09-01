f5fa72bccf40db89c7de997962d942ace254e42e
Steven Daye <dev.stevendaye@yahoo.com>
2019-09-01 03:12:11 +0000

    Added app's logo and edited some non-production code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Bolbir is based on the Algolia Search API to map result from your search term. V
 
 In Future versions, some functionalities will be added to make it more lively. People can see what others have searched for, how many have searched for the same thing and they can leave comments on what they searched for to help others searching for the same thing to know their mind on the topic.
 
-Technologies Used: React, Babel4 Webpck7.
+Technologies Used: React, Babel4 Webpack7, Jest, Enzyme.
 
 Use:
 - npm install
--- tools/publicServer.js
+++ tools/publicServer.js
@@ -5,14 +5,12 @@ import open from 'open';
 import compression from 'compression';
 import favicon from 'serve-favicon';
 
-/*eslint-disable no-console */
-
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(compression());
 app.use(express.static('public'));
-// app.use(favicon(path.join(__dirname, 'assets', 'bolbir.ico')));
+app.use(favicon(path.join(__dirname, 'assets', 'bolbir.ico')));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -22,6 +20,7 @@ app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
-    open(`http://localhost:${port}`);
+    console.log("Server running in production at https://www.bolbir.herokuapp.com");
+    // open(`http://localhost:${port}`);
   }
 });
